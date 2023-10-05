154910ababc56775ec786dbbd1ea302569ef797e
cityshin <ssk7750@naver.com>
2023-10-06 00:18:09 +0900

    Feat. 초기 설정

---
+++ app/src/main/java/com/example/meetup/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.meetup.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.meetup.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
