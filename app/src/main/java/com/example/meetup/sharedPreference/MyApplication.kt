c84ec4647501c43f59b56254f376707635db1198
cityshin <ssk7750@naver.com>
2023-10-17 16:52:47 +0900

    Feat. sharedPreference 추가

---
+++ app/src/main/java/com/example/meetup/sharedPreference/MyApplication.kt
@@ -0,0 +1,15 @@
+package com.example.meetup.sharedPreference
+
+import android.app.Application
+
+class MyApplication : Application() {
+    companion object{
+        lateinit var preferences: PreferenceUtil
+    }
+
+    override fun onCreate() {
+        preferences = PreferenceUtil(applicationContext)
+        super.onCreate()
+
+    }
+}
