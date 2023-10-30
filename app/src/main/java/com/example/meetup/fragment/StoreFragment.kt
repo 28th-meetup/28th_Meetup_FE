55e0dd30e33823b168e0e20b198818317230b50e
cityshin <ssk7750@naver.com>
2023-10-31 00:29:26 +0900

    Feat. layout list button 로직 추가

--- app/src/main/java/com/example/meetup/fragment/StoreFragment.kt
+++ app/src/main/java/com/example/meetup/fragment/StoreFragment.kt
@@ -6,9 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.meetup.R
+import android.graphics.Color
+
+import com.example.meetup.databinding.FragmentStoreBinding
 
 
 class StoreFragment : Fragment() {
+
+
+    private var _binding: FragmentStoreBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +27,51 @@ class StoreFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_store, container, false)
+
+        _binding = FragmentStoreBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.btnAll.setOnClickListener {
+
+            btnAll()
+        }
+
+        binding.btnMenuChange.setOnClickListener {
+
+
+            btnMenuChange()
+        }
+    }
+
+
+    fun btnAll() {
+        binding.btnAll.setBackgroundResource(R.drawable.store_select_border)
+
+        binding.btnAll.setTextColor(Color.parseColor("#1E1F23"))
+        binding.btnMenuChange.setBackgroundResource(R.drawable.store_not_select_border)
+        binding.btnMenuChange.setTextColor(Color.parseColor("#6E7178"))
+    }
+
+    fun btnMenuChange() {
+
+        binding.btnMenuChange.setTextColor(Color.parseColor("#1E1F23"))
+
+        binding.btnMenuChange.setBackgroundResource(R.drawable.store_select_border)
+
+
+        binding.btnAll.setTextColor(Color.parseColor("#6E7178"))
+
+        binding.btnAll.setBackgroundResource(R.drawable.store_not_select_border)
+
     }
 
 
