3c5b79d26d705b9d973ff428b12842118fe383a7
cityshin <ssk7750@naver.com>
2023-11-03 19:05:50 +0900

    Feat. bottom sheet 수정

--- app/src/main/java/com/example/meetup/fragment/ModalBottomSheetFragment.kt
+++ app/src/main/java/com/example/meetup/fragment/ModalBottomSheetFragment.kt
@@ -1,5 +1,6 @@
 package com.example.meetup.fragment
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,7 +16,9 @@ class ModalBottomSheetFragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View?{
 
-
+// 배경 어두운 효과 추가
+        val dialog = dialog
+        dialog?.window?.setDimAmount(0.5f)
 
 
         return inflater.inflate(R.layout.bottom_sheets_layout, container, false)
@@ -24,6 +27,8 @@ class ModalBottomSheetFragment : BottomSheetDialogFragment() {
     companion object {
         const val TAG = "ModalBottomSheet"
     }
+
+    override fun getTheme(): Int = R.style.AppBottomSheetDialogTheme
 
 
 
