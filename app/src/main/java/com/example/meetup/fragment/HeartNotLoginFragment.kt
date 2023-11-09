25bfb7ce6a31fcf2af4d1e78a287b6c13f2314a0
cityshin <ssk7750@naver.com>
2023-11-09 23:08:22 +0900

    Feat. 비로그인 찜 목록 추가

---
+++ app/src/main/java/com/example/meetup/fragment/HeartNotLoginFragment.kt
@@ -0,0 +1,33 @@
+package com.example.meetup.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.meetup.R
+import com.example.meetup.base.BaseFragment
+import com.example.meetup.databinding.FragmentHeartNotLoginBinding
+
+
+class HeartNotLoginFragment : BaseFragment<FragmentHeartNotLoginBinding>(R.layout.fragment_heart_not_login) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
