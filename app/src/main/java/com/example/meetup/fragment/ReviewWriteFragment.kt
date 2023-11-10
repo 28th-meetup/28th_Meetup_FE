4847a6e2e29af5c9db1ef54aa236a1eaff573090
cityshin <ssk7750@naver.com>
2023-11-10 19:09:55 +0900

    Feat. photo picker 추가

--- app/src/main/java/com/example/meetup/fragment/ReviewWriteFragment.kt
+++ app/src/main/java/com/example/meetup/fragment/ReviewWriteFragment.kt
@@ -1,10 +1,14 @@
 package com.example.meetup.fragment
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.meetup.R
 import com.example.meetup.base.BaseFragment
 import com.example.meetup.databinding.FragmentReviewWriteBinding
@@ -29,8 +33,31 @@ class ReviewWriteFragment : BaseFragment<FragmentReviewWriteBinding>(R.layout.fr
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val pickMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                // Callback is invoked after the user selects a media item or closes the
+                // photo picker.
+                if (uri != null) {
+                    Log.d("PhotoPicker", "Selected URI: $uri")
+
+//                        bitmap = BitmapFactory.decodeStream(
+//                            requireContext().contentResolver.openInputStream(uri)
+//                        )
+
+//                        binding.textView8.setText("$uri")
+
+
+
+                } else {
+                    Log.d("PhotoPicker", "No media selected")
+                }
+            }
+
         binding.btnChoosePhoto.setOnClickListener{
-            //photopicker 사용
+
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+
+
         }
         binding.imageviewStar1.setOnClickListener {
             binding.imageviewStar1.setImageResource(R.drawable.ic_star)
