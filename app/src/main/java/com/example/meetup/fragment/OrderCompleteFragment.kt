283d4ec4ec8e798381a8e673406a93d1b70eff58
PIYUJIN <yjyj020201@gmail.com>
2023-11-10 22:38:42 +0900

    Feat. 주문 - 주문완료 화면 구성 및 화면 전환 기능 구현
    주문 - 주문완료 화면 구성 및 화면 전환 기능 구현
    
    Fixes : #9

---
+++ app/src/main/java/com/example/meetup/fragment/OrderCompleteFragment.kt
@@ -0,0 +1,54 @@
+package com.example.meetup.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.meetup.R
+import com.example.meetup.activity.HomeActivity
+import com.example.meetup.databinding.FragmentOrderCompleteBinding
+
+class OrderCompleteFragment : Fragment() {
+
+    lateinit var binding: FragmentOrderCompleteBinding
+    lateinit var homeActivity: HomeActivity
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentOrderCompleteBinding.inflate(inflater)
+        homeActivity = activity as HomeActivity
+
+        initView()
+
+        binding.run {
+            buttonHome.setOnClickListener {
+                val homeFragment = HomeFragment()
+
+                val transaction = homeActivity.manager.beginTransaction()
+                transaction.replace(R.id.frameArea, homeFragment)
+                transaction.commit()
+            }
+
+            buttonOrderList.setOnClickListener {
+                // 마이페이지 주문내역 페이지로 이동
+//                val homeFragment = HomeFragment()
+//
+//                val transaction = homeActivity.manager.beginTransaction()
+//                transaction.replace(R.id.frameArea, homeFragment)
+//                transaction.commit()
+            }
+        }
+
+        return binding.root
+    }
+
+    fun initView() {
+        binding.run {
+            homeActivity.hideBottomNavigation(true)
+        }
+    }
+}
