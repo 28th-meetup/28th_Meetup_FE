a3c5d8b8fed4ea2692c3988267161fc39d5b2110
cityshin <ssk7750@naver.com>
2023-11-12 13:33:13 +0900

    Feat. chatting adapter 연결

---
+++ app/src/main/java/com/example/meetup/model/ChattingListResponseModel.kt
@@ -0,0 +1,9 @@
+package com.example.meetup.model
+
+data class ChattingListResponseModel(
+    var imageview_chatting_image : String,
+    var textview_chatting_name : String,
+    var textview_chatting_content : String,
+    var textview_chatting_time : String,
+    var textview_chatting_num : String
+)
