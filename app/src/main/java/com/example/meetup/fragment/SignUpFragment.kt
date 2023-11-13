a252605438b32e7b1295895cbe16668676e9ffae
PIYUJIN <yjyj020201@gmail.com>
2023-11-13 14:16:54 +0900

    Feat. 회원가입 화면 toolbar 및 spinner 기능 구현
    회원가입 화면 toolbar 및 spinner 기능 구현
    
    Fixes : #12

--- app/src/main/java/com/example/meetup/fragment/SignUpFragment.kt
+++ app/src/main/java/com/example/meetup/fragment/SignUpFragment.kt
@@ -1,60 +1,92 @@
 package com.example.meetup.fragment
 
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.meetup.R
+import com.example.meetup.activity.AuthActivity
+import com.example.meetup.databinding.FragmentSignUpBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentSignUpBinding
+    lateinit var authActivity: AuthActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+
+        binding = FragmentSignUpBinding.inflate(inflater)
+        authActivity = activity as AuthActivity
+
+        initView()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignUpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    fun initView() {
+        binding.run {
+            toolbar.run {
+                title = "이메일로 회원가입"
+
+                // back 버튼 설정
+                setNavigationIcon(R.drawable.ic_close)
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    navigationIcon?.colorFilter =
+                        BlendModeColorFilter(Color.DKGRAY, BlendMode.SRC_ATOP)
+                } else {
+                    navigationIcon?.setColorFilter(Color.DKGRAY, PorterDuff.Mode.SRC_ATOP)
+                }
+
+                setNavigationOnClickListener {
+                    // 유저 인입경로별 뒤로가기 기능 구현
+                    authActivity.finish()
                 }
             }
+
+            textviewEmailError.visibility = View.GONE
+            textviewPasswordError.visibility = View.GONE
+            textviewPasswordCheckError.visibility = View.GONE
+            textviewNickNameError.visibility = View.GONE
+        }
+
+        var loactionSpinner = binding.spinnerLocation	// spinner
+        var locationArray = resources.getStringArray(R.array.location_array)	// 배열
+        setSpinner(loactionSpinner, locationArray)	// 스피너 설정
     }
+
+    // 스피너 설정
+    private fun setSpinner(spinner: Spinner, array: Array<String>) {
+        var adapter = object : ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_dropdown_item_1line
+        ) { override fun getCount(): Int =  super.getCount() - 1 }  // array에서 hint 안 보이게 하기
+        adapter.addAll(array.toMutableList())   // 배열 추가
+        spinner.adapter = adapter               // 어댑터 달기
+        spinner.setSelection(8)    // 스피너 초기값=hint
+    }
+
+    //선택시 결과를 출력해주는 함수
+//    fun setupSpinnerHandler(){
+//
+//        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+//            override fun onItemSelected(p0: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//                textView.text = "선택됨: $position ${spinner.getItemAtPosition(position)}"
+//            }
+//
+//            override fun onNothingSelected(p0: AdapterView<*>?) {
+//            }
+//        }
+//    }
 }
