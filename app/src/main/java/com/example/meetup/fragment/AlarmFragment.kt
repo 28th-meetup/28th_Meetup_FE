46b5847c4c50927e44792293c1ceea762ab004a4
cityshin <ssk7750@naver.com>
2023-11-14 17:04:23 +0900

    Feat. alarm fragment 생성

---
+++ app/src/main/java/com/example/meetup/fragment/AlarmFragment.kt
@@ -0,0 +1,37 @@
+package com.example.meetup.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.meetup.R
+import com.example.meetup.base.BaseFragment
+import com.example.meetup.databinding.FragmentAlarmBinding
+
+
+class AlarmFragment : BaseFragment<FragmentAlarmBinding>(R.layout.fragment_alarm) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnBack.setOnClickListener {
+            fragmentManager?.popBackStack()
+        }
+    }
+
+}
