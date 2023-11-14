a7cc167ce43c857fb416bcc2a43ea1be27f508ba
cityshin <ssk7750@naver.com>
2023-11-14 14:10:46 +0900

    Feat. adapter 연결

---
+++ app/src/main/java/com/example/meetup/adapter/MenuListAdapter.kt
@@ -0,0 +1,67 @@
+package com.example.meetup.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.meetup.R
+import com.example.meetup.model.MenuListResponseModel
+
+
+class MenuListAdapter(
+    private var menuList: ArrayList<MenuListResponseModel>,
+//    private val itemClickListener1: (Int) -> Unit,
+//    private val itemClickListener2: (Int) -> Unit,
+
+    ) : RecyclerView.Adapter<MenuListAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuListAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recyclerview_item_menu_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MenuListAdapter.ViewHolder, position: Int) {
+        val item = menuList[position]
+        holder.bind(item)
+
+
+        //클릭 이벤트
+//        holder.btn_edit.setOnClickListener {
+//
+//            itemClickListener1.invoke(position)
+//        }
+//
+//        holder.btn_delete.setOnClickListener {
+//            itemClickListener2.invoke(position)
+//
+//        }
+    }
+
+    override fun getItemCount(): Int {
+        return menuList.size
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        var imageview_menu_image: ImageView = view.findViewById(R.id.imageview_menu_image)
+        var textview_menu_name: TextView = view.findViewById(R.id.textview_menu_name)
+        var textview_menu_category: TextView = view.findViewById(R.id.textview_menu_category)
+        var textview_menu_price: TextView = view.findViewById(R.id.textview_menu_price)
+
+        var btn_edit : ImageView = view.findViewById(R.id.btn_edit)
+        var btn_delete : ImageView = view.findViewById(R.id.btn_delete)
+
+
+        fun bind(item: MenuListResponseModel) {
+
+            textview_menu_name.text = item.menuName
+            textview_menu_category.text = item.menuCategory
+            textview_menu_price.text = item.menuPrice
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/meetup/model/MenuListResponseModel.kt
@@ -0,0 +1,8 @@
+package com.example.meetup.model
+
+data class MenuListResponseModel(
+    var menuImage : String,
+    var menuName: String,
+    var menuCategory: String,
+    var menuPrice : String
+)
