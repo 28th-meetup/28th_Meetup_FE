a61fe602d05b6821097a889db0927453a16e32e7
PIYUJIN <yjyj020201@gmail.com>
2023-11-17 13:07:53 +0900

    Feat. 회원가입 - 주소설정 서버 연결 및 예외 처리 기능 구현
    회원가입 - 주소설정 서버 연결 및 예외 처리 기능 구현
    
    Fixes : #12

--- app/src/main/java/com/example/meetup/fragment/SignUpAddressCheckFragment.kt
+++ app/src/main/java/com/example/meetup/fragment/SignUpAddressCheckFragment.kt
@@ -1,21 +1,38 @@
 package com.example.meetup.fragment
 
+import android.content.Intent
 import android.graphics.BlendMode
 import android.graphics.BlendModeColorFilter
 import android.graphics.Color
 import android.graphics.PorterDuff
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.meetup.R
+import com.example.meetup.activity.AuthActivity
+import com.example.meetup.activity.MainActivity
 import com.example.meetup.databinding.FragmentSignUpAddressCheckBinding
+import com.example.meetup.dialog.DialogSignUpAddress
+import com.example.meetup.model.AddressesResponseModel
+import com.example.meetup.model.AddressesValidResponseModel
+import com.example.meetup.retrofit2.RetrofitInstance
+import com.example.meetup.sharedPreference.MyApplication
+import com.google.android.material.snackbar.Snackbar
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpAddressCheckFragment : Fragment() {
 
     lateinit var binding: FragmentSignUpAddressCheckBinding
+    lateinit var authActivity: AuthActivity
+
+    private val APIS = RetrofitInstance.retrofitInstance().create(com.example.meetup.retrofit2.APIS::class.java)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,8 +40,24 @@ class SignUpAddressCheckFragment : Fragment() {
     ): View? {
 
         binding = FragmentSignUpAddressCheckBinding.inflate(inflater)
+        authActivity = activity as AuthActivity
 
         initView()
+
+        binding.run {
+            buttonSignUp.setOnClickListener {
+                // 사용자 주소 설정 API
+                setAddresses()
+            }
+
+            buttonBack.setOnClickListener {
+                val addressFragment = SignUpAddressDetailFragment()
+
+                val transaction = authActivity.supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.container_auth, addressFragment)
+                transaction.commit()
+            }
+        }
 
         return binding.root
     }
@@ -35,7 +68,7 @@ class SignUpAddressCheckFragment : Fragment() {
                 title = "주소 입력"
 
                 // back 버튼 설정
-                setNavigationIcon(R.drawable.ic_close)
+                setNavigationIcon(R.drawable.ic_back)
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                     navigationIcon?.colorFilter =
@@ -45,10 +78,64 @@ class SignUpAddressCheckFragment : Fragment() {
                 }
 
                 setNavigationOnClickListener {
-                    // 유저 인입경로별 뒤로가기 기능 구현
+                    val addressFragment = SignUpAddressDetailFragment()
 
+                    val transaction = authActivity.supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.container_auth, addressFragment)
+                    transaction.commit()
                 }
             }
+
+            var region = resources.getStringArray(R.array.location_array).get(MyApplication.address.globalRegionId.toInt())
+            textviewLocation1Value.text = "${region}"
+            textviewLocation2Value.text = "${MyApplication.address.address}"
+            textviewLocation3Value.text = "${MyApplication.address.detailAddress}"
+            textviewLocation4Value.text = "${MyApplication.address.postalCode}"
         }
+    }
+
+    fun setAddresses() {
+
+        Log.d("밋업", "사용자 주소 설정 : ${MyApplication.address}")
+
+        APIS.setAddresses(MyApplication.address).enqueue(object :
+            Callback<AddressesResponseModel> {
+            override fun onResponse(
+                call: Call<AddressesResponseModel>,
+                response: Response<AddressesResponseModel>
+            ) {
+                if (response.isSuccessful) {
+                    // 정상적으로 통신이 성공된업 경우
+                    var result: AddressesResponseModel? = response.body()
+                    Log.d("##", "onResponse 성공: " + result?.toString())
+
+                    authActivity.finish()
+
+                    val mainIntent = Intent(authActivity,MainActivity::class.java)
+                    startActivity(mainIntent)
+
+                    Toast.makeText(requireContext(), "회원가입이 완료되었습니다.", Toast.LENGTH_LONG).show()
+                } else {
+                    // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
+                    Log.d("##", "onResponse 실패: " + response.code())
+                    Log.d("##", "onResponse 실패: " + response.message())
+                    Log.d("##", "onResponse 실패: " + response.body()?.message.toString())
+                    Log.d("##", "onResponse 실패: " + response.errorBody())
+
+                    if (response.code() == 400) {
+                        Snackbar.make(
+                            binding.root,
+                            "존재하지 않는 정보입니다",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<AddressesResponseModel>, t: Throwable) {
+                // 통신 실패
+                Log.d("##", "onFailure 에러: " + t.message.toString());
+            }
+        })
     }
 }
--- app/src/main/java/com/example/meetup/fragment/SignUpAddressDetailFragment.kt
+++ app/src/main/java/com/example/meetup/fragment/SignUpAddressDetailFragment.kt
@@ -11,12 +11,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.meetup.R
+import com.example.meetup.activity.AuthActivity
 import com.example.meetup.databinding.FragmentSignUpAddressCheckBinding
 import com.example.meetup.databinding.FragmentSignUpAddressDetailBinding
+import com.example.meetup.sharedPreference.MyApplication
 
 class SignUpAddressDetailFragment : Fragment() {
 
     lateinit var binding: FragmentSignUpAddressDetailBinding
+    lateinit var authActivity: AuthActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +27,21 @@ class SignUpAddressDetailFragment : Fragment() {
     ): View? {
 
         binding = FragmentSignUpAddressDetailBinding.inflate(inflater)
+        authActivity = activity as AuthActivity
 
         initView()
+
+        binding.run {
+            buttonCheckAddress.setOnClickListener {
+                MyApplication.address.detailAddress = edittextLocation2.text.toString()
+
+                val addressCheckFragment = SignUpAddressCheckFragment()
+
+                val transaction = authActivity.supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.container_auth, addressCheckFragment)
+                transaction.commit()
+            }
+        }
 
         return binding.root
     }
@@ -36,7 +52,7 @@ class SignUpAddressDetailFragment : Fragment() {
                 title = "주소 입력"
 
                 // back 버튼 설정
-                setNavigationIcon(R.drawable.ic_close)
+                setNavigationIcon(R.drawable.ic_back)
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                     navigationIcon?.colorFilter =
@@ -46,10 +62,15 @@ class SignUpAddressDetailFragment : Fragment() {
                 }
 
                 setNavigationOnClickListener {
-                    // 유저 인입경로별 뒤로가기 기능 구현
+                    val addressFragment = SignUpAddressFragment()
 
+                    val transaction = authActivity.supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.container_auth, addressFragment)
+                    transaction.commit()
                 }
             }
+
+            edittextPostNum.setText("${MyApplication.address.postalCode}")
         }
     }
 }
