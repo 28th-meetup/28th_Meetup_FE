84b1931835ff0b07a25903242c506fce5e4416f6
cityshin <ssk7750@naver.com>
2023-11-18 22:12:47 +0900

    Feat. api 추가

--- app/src/main/java/com/example/meetup/model/store/PostStoreDtoRequestModel.kt
+++ app/src/main/java/com/example/meetup/model/store/PostStoreDtoRequestModel.kt
@@ -1,8 +1,16 @@
 package com.example.meetup.model.store
 
 data class PostStoreDtoRequestModel(
-    var name: String,
-    var description: String,
-    var koreanYn: Boolean,
-    var minOrderAmount: Long
+    val name: String,
+    val minOrderAmount: Long,
+    val description: String,
+    val countryPhoneCode: String,
+    val phoneNum: String,
+    val globalRegionId: Long,
+    val address: String,
+    val detailAddress: String,
+    val deliveryRegion: String,
+    val operationTime: String,
+    val foodChangeYn: Boolean,
+    val koreanYn: Boolean,
 )
