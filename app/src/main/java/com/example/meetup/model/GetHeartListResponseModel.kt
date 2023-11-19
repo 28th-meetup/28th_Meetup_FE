a3165581369190b459b67c6884999572c66f2124
cityshin <ssk7750@naver.com>
2023-11-19 16:39:09 +0900

    Feat. 찜 목록 수정

---
+++ app/src/main/java/com/example/meetup/model/GetHeartListResponseModel.kt
@@ -0,0 +1,24 @@
+package com.example.meetup.model
+
+data class GetHeartListResponseModel(
+
+val isSuccess: Boolean,
+val code: Long,
+val message: String,
+val result: GetHeartListResponseModelResult,
+)
+
+data class GetHeartListResponseModelResult(
+    val stores: ArrayList<GetHeartListResponseModelStore>,
+)
+
+data class GetHeartListResponseModelStore(
+    val id: Long,
+    val name: String,
+    val description: String,
+    val koreanYn: Boolean,
+    val avgRate: Long,
+    val minOrderAmount: Long,
+    val images: ArrayList<String>,
+)
+
---
+++ app/src/main/java/com/example/meetup/viewmodel/HeartViewModel.kt
@@ -0,0 +1,60 @@
+package com.example.meetup.viewmodel
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.meetup.model.GetHeartListResponseModel
+import com.example.meetup.model.store.GetStoreListResponseModel
+import com.example.meetup.retrofit2.APIS
+import com.example.meetup.retrofit2.RetrofitInstance
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class HeartViewModel:ViewModel() {
+
+    private lateinit var API : APIS
+    private var _heartList = MutableLiveData<GetHeartListResponseModel>()
+    var heartList : LiveData<GetHeartListResponseModel> = _heartList
+
+    fun getHeartList (context: Context) {
+        API = RetrofitInstance.retrofitInstance().create(APIS::class.java)
+
+        val tokenManager = com.example.meetup.sharedPreference.TokenManager(context)   //가게 목록 가져오기
+
+//        val accessToken = MyApplication.preferences.getString("accessToken", "")
+
+        Log.d("tokenManager", tokenManager.getAccessToken().toString())
+        viewModelScope.launch {
+            try{
+                API.getHeartList(tokenManager.getAccessToken().toString()).enqueue(
+                    object : Callback<GetHeartListResponseModel> {
+
+                        override fun onResponse(call: Call<GetHeartListResponseModel>, response: Response<GetHeartListResponseModel>) {
+                            if (response.isSuccessful) {
+
+//                                _storeList.value = response.body()?.result?.stores
+                                _heartList.value = response.body()
+
+
+                                Log.d("GetHeartListResponseModel : " , " success , ${response.body().toString()}")
+                            } else {
+
+                                Log.d("GetHeartListResponseModel Response : ", "fail 1 ${response.body().toString()} , ${response.message()}, ${response.errorBody().toString()}")
+                            }
+                        }
+
+                        override fun onFailure(call: Call<GetHeartListResponseModel>, t: Throwable) {
+                            Log.d("GetHeartListResponseModel Response : ", " fail 2 , ${t.message.toString()}")
+                        }
+                    })
+            } catch (e:Exception) {
+                Log.d("GetHeartListResponseModel response : ", " fail 3 , ${e.message}")
+            }
+        }
+    }
+}
