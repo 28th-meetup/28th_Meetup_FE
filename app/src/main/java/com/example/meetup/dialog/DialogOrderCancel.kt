2cdb19661fdd18225f3ed0433362f7fe5cb8a28f
PIYUJIN <yjyj020201@gmail.com>
2023-11-21 02:27:54 +0900

    Feat. 판매자 - 주문관리화면 서버 연결 기능 구현
    판매자 - 주문관리화면 서버 연결 기능 구현
    
    Fixes : #17

--- app/src/main/java/com/example/meetup/dialog/DialogOrderCancel.kt
+++ app/src/main/java/com/example/meetup/dialog/DialogOrderCancel.kt
@@ -1,8 +1,10 @@
 package com.example.meetup.dialog
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,21 +12,31 @@ import android.view.Window
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
 import com.example.meetup.R
-import com.example.meetup.activity.CertificationActivity
+import com.example.meetup.activity.HomeActivity
 import com.example.meetup.databinding.ActivityHomeBinding
 import com.example.meetup.databinding.DialogSellerOrderCancelBinding
-import com.example.meetup.databinding.DialogSignUpBinding
+import com.example.meetup.model.BasicResponseModel
+import com.example.meetup.model.OrderPreviewResponseList
+import com.example.meetup.model.SignInResponseModel
+import com.example.meetup.retrofit2.RetrofitInstance
+import com.example.meetup.sharedPreference.TokenManager
+import com.google.android.material.snackbar.Snackbar
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 interface OrderCancelDialogInterface {
     fun onClickYesButton(id: Int)
 }
-class DialogOrderCancel(var manager: FragmentManager) : DialogFragment() {
+class DialogOrderCancel(var manager: FragmentManager, var orderHistory: OrderPreviewResponseList) : DialogFragment() {
 
     // 뷰 바인딩 정의
     private var _binding: DialogSellerOrderCancelBinding? = null
     private val binding get() = _binding!!
 
     private var confirmDialogInterface: OrderCancelDialogInterface? = null
+
+    private val APIS = RetrofitInstance.retrofitInstance().create(com.example.meetup.retrofit2.APIS::class.java)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,8 +48,18 @@ class DialogOrderCancel(var manager: FragmentManager) : DialogFragment() {
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
         dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
 
+        binding.run {
+            if(orderHistory.orderDetailPreviewList.size > 1) {
+                textviewOrderCancelFoodName.text = orderHistory.orderDetailPreviewList.get(0).foodName + " 외 ${orderHistory.orderDetailPreviewList.size-1}개"
+            } else {
+                textviewOrderCancelFoodName.text = orderHistory.orderDetailPreviewList.get(0).foodName
+            }
+            textviewOrderCancelTime.text = orderHistory.orderedAt
+            textviewOrderCancelAddress.text = orderHistory.addressAndPostalCode
+        }
+
         binding.buttonOrderCancel.setOnClickListener {
-            dismiss()
+            cancelOrder()
         }
 
 
@@ -58,5 +80,42 @@ class DialogOrderCancel(var manager: FragmentManager) : DialogFragment() {
         var activityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
 
         activityHomeBinding.root.setBackgroundResource(R.drawable.blur_background)
+    }
+
+    fun cancelOrder() {
+
+        var tokenManager = TokenManager(requireContext())
+
+        APIS.setOrderStatus(tokenManager.getAccessToken().toString(), orderHistory.orderId.toInt(), "rejected").enqueue(object :
+            Callback<BasicResponseModel> {
+            override fun onResponse(
+                call: Call<BasicResponseModel>,
+                response: Response<BasicResponseModel>
+            ) {
+                if (response.isSuccessful) {
+                    // 정상적으로 통신이 성공된 경우
+                    var result: BasicResponseModel? = response.body()
+                    Log.d("##", "onResponse 성공: " + result?.toString())
+                    dismiss()
+                } else {
+                    // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
+                    Log.d("##", "onResponse 실패: " + response.code())
+                    Log.d("##", "onResponse 실패: " + response.body())
+
+                    if (response.code() == 400) {
+                        Snackbar.make(
+                            binding.root,
+                            "다시 시도해주세요.",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponseModel>, t: Throwable) {
+                // 통신 실패
+                Log.d("##", "onFailure 에러: " + t.message.toString());
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/meetup/viewmodel/SellerOrderHistoryViewModel.kt
@@ -0,0 +1,101 @@
+package com.example.meetup.viewmodel
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.meetup.model.CategoryIdResponseModel
+import com.example.meetup.model.Food
+import com.example.meetup.model.OrderDetailPreviewList
+import com.example.meetup.model.OrderHistoryResult
+import com.example.meetup.model.OrderPreviewResponseList
+import com.example.meetup.model.SellerOrderHistoryResponseModel
+import com.example.meetup.retrofit2.RetrofitInstance
+import com.example.meetup.sharedPreference.TokenManager
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class SellerOrderHistoryViewModel : ViewModel() {
+    private val APIS = RetrofitInstance.retrofitInstance().create(com.example.meetup.retrofit2.APIS::class.java)
+
+    var orderHistoryList = MutableLiveData<MutableList<OrderPreviewResponseList>>()
+    var orderCount = MutableLiveData<Long>()
+
+
+    init {
+        orderHistoryList.value = mutableListOf<OrderPreviewResponseList>()
+    }
+
+    fun getSellerOrderHistory(context: Context, orderStatus : String) {
+
+        var tempZeroList = mutableListOf<OrderPreviewResponseList>()
+        var tempPreviewList = mutableListOf<OrderPreviewResponseList>()
+        var tokenManager = TokenManager(context)
+
+        APIS.getSellerOrderHistory(tokenManager.getAccessToken().toString(), orderStatus).enqueue(object :
+            Callback<SellerOrderHistoryResponseModel> {
+            override fun onResponse(
+                call: Call<SellerOrderHistoryResponseModel>,
+                response: Response<SellerOrderHistoryResponseModel>
+            ) {
+                if (response.isSuccessful) {
+                    // 정상적으로 통신이 성공된 경우
+                    var result: SellerOrderHistoryResponseModel? = response.body()
+                    Log.d("##", "onResponse 성공: " + result?.toString())
+                    if(result?.result!!.orderPreviewResponseList.size == 0) {
+                        Toast.makeText(context, "해당 카테고리에 해당하는 메뉴가 없습니다.", Toast.LENGTH_SHORT).show()
+                        orderCount.value = 0
+                        orderHistoryList.value = tempZeroList
+                    } else {
+                        orderCount.value = result.result!!.orderCount
+                        for (i in 0 until result.result!!.orderPreviewResponseList.size) {
+                            var tempDetailList = mutableListOf<OrderDetailPreviewList>()
+                            var id = result?.result!!.orderPreviewResponseList.get(i).orderId
+                            var userName = result?.result!!.orderPreviewResponseList.get(i).userName
+                            var addressAndPostalCode = result?.result!!.orderPreviewResponseList.get(i).addressAndPostalCode
+                            var detailAddress = result?.result!!.orderPreviewResponseList.get(i).detailAddress
+                            var selectedOption = result?.result!!.orderPreviewResponseList.get(i).selectedOption
+                            var orderedAt = result?.result!!.orderPreviewResponseList.get(i).orderedAt
+                            for (j in 0 until result.result!!.orderPreviewResponseList.get(i).orderDetailPreviewList.size) {
+                                var orderDeatailId = result?.result!!.orderPreviewResponseList.get(i).orderDetailPreviewList.get(j).orderDetailId
+                                var foodId = result?.result!!.orderPreviewResponseList.get(i).orderDetailPreviewList.get(j).foodId
+                                var foodName = result?.result!!.orderPreviewResponseList.get(i).orderDetailPreviewList.get(j).foodName
+                                var foodOptionId = result?.result!!.orderPreviewResponseList.get(i).orderDetailPreviewList.get(j).foodOptionId
+                                var foodOptionName = result?.result!!.orderPreviewResponseList.get(i).orderDetailPreviewList.get(j).foodOptionName
+                                var orderCount = result?.result!!.orderPreviewResponseList.get(i).orderDetailPreviewList.get(j).orderCount
+
+                                var detail = OrderDetailPreviewList(orderDeatailId, foodId, foodName, foodOptionId, foodOptionName, orderCount)
+                                tempDetailList.add(detail)
+                            }
+
+                            var orderHistory = OrderPreviewResponseList(id, userName, addressAndPostalCode, detailAddress, selectedOption, orderedAt, tempDetailList)
+
+                            tempPreviewList.add(orderHistory)
+                        }
+
+                        orderHistoryList.value = tempPreviewList
+
+                        Log.d("밋업", "${orderHistoryList.value}")
+                    }
+                } else {
+                    // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
+                    Log.d("##", "onResponse 실패: " + response.code())
+                    Log.d("##", "onResponse 실패: " + response.message())
+
+                    if (response.code() == 400) {
+                        Toast.makeText(context, "존재하지 않는 가게입니다.", Toast.LENGTH_SHORT).show()
+                        orderCount.value = 0
+                        orderHistoryList.value = tempZeroList
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<SellerOrderHistoryResponseModel>, t: Throwable) {
+                // 통신 실패
+                Log.d("##", "onFailure 에러: " + t.message.toString());
+            }
+        })
+    }
+}
