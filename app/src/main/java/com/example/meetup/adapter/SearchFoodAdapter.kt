ec75d3e56b5e11ca00d319073a8192b55f833808
PIYUJIN <yjyj020201@gmail.com>
2023-11-21 11:49:36 +0900

    Feat. 검색화면 서버 연결 기능 구현
    검색화면 서버 연결 기능 구현
    
    Fixes : #34

---
+++ app/src/main/java/com/example/meetup/adapter/SearchFoodAdapter.kt
@@ -0,0 +1,121 @@
+package com.example.meetup.adapter
+
+import android.content.Context
+import android.graphics.Rect
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.meetup.R
+import com.example.meetup.databinding.RowSetBinding
+import com.example.meetup.fragment.MenuFragment
+import com.example.meetup.model.RecentSetFoodList
+import com.example.meetup.model.SearchFood
+import com.example.meetup.sharedPreference.MyApplication
+import com.example.meetup.viewmodel.FoodMenuDetailViewModel
+import com.example.meetup.viewmodel.SearchViewModel
+
+class SearchFoodAdapter(var manager: FragmentManager, var activity: ViewModelStoreOwner, var foodList: List<SearchFood>) :
+    RecyclerView.Adapter<SearchFoodAdapter.SearchFoodViewHolder>() {
+    private var onItemClickListener: ((Int) -> Unit)? = null
+    private var context: Context? = null
+
+    lateinit var viewModel: FoodMenuDetailViewModel
+
+    fun setOnItemClickListener(listener: (Int) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchFoodViewHolder {
+        context = parent.context
+        val binding =
+            RowSetBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        viewModel = ViewModelProvider(activity)[FoodMenuDetailViewModel::class.java]
+
+        binding.root.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+
+        return SearchFoodViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SearchFoodViewHolder, position: Int) {
+        Glide.with(context!!).load(foodList.get(position).image).into(holder.image)
+        holder.storeName.text = "${foodList.get(position).storeId}"
+//        holder.review.text = "⭐️ ${foodList.get(position).avgRate}"
+        holder.foodName.text = "${foodList.get(position).name}"
+        if (foodList.get(position).dollarPrice.toInt() == 0) {
+            holder.price.text = "${foodList.get(position).canadaPrice}"
+        } else {
+            holder.price.text = "$ ${foodList.get(position).dollarPrice}"
+        }
+    }
+
+    override fun getItemCount() = foodList.size
+
+    inner class SearchFoodViewHolder(val binding: RowSetBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val image = binding.imageview
+        val storeName = binding.textviewStoreName
+        val review = binding.textviewReview
+        val foodName = binding.textviewFoodName
+        val price = binding.textviewPrice
+
+        init {
+            binding.root.setOnClickListener {
+
+                MyApplication.foodId = foodList.get(adapterPosition).id.toInt()
+
+                viewModel.getFoodMenuInfo(context!!, MyApplication.foodId)
+
+                Handler().postDelayed({
+                    val menuFragment = MenuFragment()
+
+                    val transaction = manager.beginTransaction()
+                    transaction.replace(R.id.frameArea, menuFragment)
+                    transaction.addToBackStack("")
+                    transaction.commit()
+                }, 1000)
+                true
+            }
+        }
+    }
+
+    internal class GridSpacingItemDecoration(
+        private val spanCount: Int,
+        private val spacing: Int,
+        private val includeEdge: Boolean
+    ) :
+        RecyclerView.ItemDecoration() {
+
+        override fun getItemOffsets(
+            outRect: Rect,
+            view: View,
+            parent: RecyclerView,
+            state: RecyclerView.State
+        ) {
+            val position = parent.getChildAdapterPosition(view) // 아이템의 위치를 가져옴
+            val column = position % spanCount // 아이템이 속한 열을 계산
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount
+                outRect.right = (column + 1) * spacing / spanCount
+            } else {
+                outRect.left = column * spacing / spanCount
+                outRect.right = spacing - (column + 1) * spacing / spanCount
+            }
+
+            if (position < spanCount) {
+                outRect.top = spacing // 첫 번째 행에는 위쪽 간격을 설정
+            }
+            outRect.bottom = spacing // 아래쪽 간격을 설정
+        }
+    }
+}
---
+++ app/src/main/java/com/example/meetup/viewmodel/SearchViewModel.kt
@@ -0,0 +1,108 @@
+package com.example.meetup.viewmodel
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.meetup.model.ProcessingFoodList
+import com.example.meetup.model.SearchFood
+import com.example.meetup.model.SearchResponseModel
+import com.example.meetup.model.StoreDto
+import com.example.meetup.retrofit2.RetrofitInstance
+import com.example.meetup.sharedPreference.TokenManager
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class SearchViewModel : ViewModel() {
+    private val APIS = RetrofitInstance.retrofitInstance().create(com.example.meetup.retrofit2.APIS::class.java)
+
+    var searchStoreList = MutableLiveData<MutableList<StoreDto>>()
+    var searchFoodList = MutableLiveData<MutableList<SearchFood>>()
+    var isExist = MutableLiveData<Boolean>()
+
+
+    init {
+        searchStoreList.value = mutableListOf<StoreDto>()
+        searchFoodList.value = mutableListOf<SearchFood>()
+    }
+
+    fun getSearchList(context: Context, keyword: String) {
+
+        var tempZeroList = mutableListOf<ProcessingFoodList>()
+        var tempStoreList = mutableListOf<StoreDto>()
+        var tempFoodList = mutableListOf<SearchFood>()
+
+        var tokenManager = TokenManager(context)
+
+        APIS.search(tokenManager.getAccessToken().toString(), keyword, "bookmark", "ASC").enqueue(object :
+            Callback<SearchResponseModel> {
+            override fun onResponse(
+                call: Call<SearchResponseModel>,
+                response: Response<SearchResponseModel>
+            ) {
+                if (response.isSuccessful) {
+                    // 정상적으로 통신이 성공된 경우
+                    var result: SearchResponseModel? = response.body()
+                    Log.d("##", "onResponse 성공: " + result?.toString())
+                    // 가게 검색 결과
+                    for (i in 0 until result?.result!!.stores.size) {
+                        var tempImageList = mutableListOf<String>()
+                        var storeId = result?.result!!.stores.get(i).storeDto.id
+                        var storeName = result?.result!!.stores.get(i).storeDto.name
+                        var description = result?.result!!.stores.get(i).storeDto.description
+                        var iskoreanStore = result?.result!!.stores.get(i).storeDto.koreanYn
+                        var avgRate = result?.result!!.stores.get(i).storeDto.avgRate
+                        var minOrderAmount = result?.result!!.stores.get(i).storeDto.minOrderAmount
+                        for(j in 0 until result?.result!!.stores.get(i).storeDto.images.size) {
+                            var images = result?.result!!.stores.get(i).storeDto.images.get(j)
+                            tempImageList.add(images)
+                        }
+
+                        var searchStore = StoreDto(storeId, storeName, description, iskoreanStore, avgRate, minOrderAmount, tempImageList)
+
+                        tempStoreList.add(searchStore)
+                    }
+                    searchStoreList.value = tempStoreList
+                    Log.d("밋업", "${searchStoreList.value}")
+
+                    // 음식 검색 결과
+                    for (i in 0 until result?.result!!.foods.size) {
+                        var foodId = result?.result!!.foods.get(i).id
+                        var storeId = result?.result!!.foods.get(i).storeId
+                        var categoryId = result?.result!!.foods.get(i).categoryId
+                        var name = result?.result!!.foods.get(i).name
+                        var dollarPrice = result?.result!!.foods.get(i).dollarPrice
+                        var canadaPrice = result?.result!!.foods.get(i).canadaPrice
+                        var description = result?.result!!.foods.get(i).description
+                        var image = result?.result!!.foods.get(i).image
+
+                        var searchFood = SearchFood(foodId, storeId, categoryId, name, dollarPrice, canadaPrice, description, image)
+
+                        tempFoodList.add(searchFood)
+                    }
+                    searchFoodList.value = tempFoodList
+                    Log.d("밋업", "${searchFoodList.value}")
+
+                    isExist.value =
+                        !(searchFoodList.value!!.size == 0 && searchStoreList.value!!.size == 0)
+                } else {
+                    // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
+                    Log.d("##", "onResponse 실패: " + response.code())
+                    Log.d("##", "onResponse 실패: " + response.message())
+
+                    if (response.code() == 400) {
+                        Toast.makeText(context, "존재하지 않는 가게입니다.", Toast.LENGTH_SHORT).show()
+                        isExist.value = false
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<SearchResponseModel>, t: Throwable) {
+                // 통신 실패
+                Log.d("##", "onFailure 에러: " + t.message.toString());
+            }
+        })
+    }
+}
