589e7b863275bddececdaad83f7aeb4a9b20f598
cityshin <ssk7750@naver.com>
2023-11-21 12:03:50 +0900

    Fix. api

--- app/src/main/java/com/example/meetup/fragment/StoreDetailInfoFragment.kt
+++ app/src/main/java/com/example/meetup/fragment/StoreDetailInfoFragment.kt
@@ -5,24 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.meetup.R
 import com.example.meetup.base.BaseFragment
+import com.example.meetup.databinding.FragmentStoreDetailBinding
 import com.example.meetup.databinding.FragmentStoreDetailInfoBinding
+import com.example.meetup.retrofit2.APIS
+import com.example.meetup.sharedPreference.MyApplication
+import com.example.meetup.viewmodel.StoreListViewModel
 
 
-class StoreDetailInfoFragment : BaseFragment<FragmentStoreDetailInfoBinding>(R.layout.fragment_store_detail_info) {
+class StoreDetailInfoFragment : Fragment() {
+    private lateinit var viewModel: StoreListViewModel
+    private lateinit var API: APIS
+    private var _binding: FragmentStoreDetailInfoBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    var storeId = 0L
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+
+        _binding = FragmentStoreDetailInfoBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+
+        viewModel = ViewModelProvider(requireActivity()).get(StoreListViewModel::class.java)
+        storeId = MyApplication.preferences.getString("storeId", "").toLong()
+
+
+
+        viewModel.getStoreDetail(requireContext(), storeId)
+
+        viewModel.storeDetail.observe(viewLifecycleOwner){
+
+
+            binding.textviewStoreName.text = it.result.storeDto.name
+            binding.textviewStoreInfo.text = it.result.storeDto.description
+
+        }
+
+
+        return view
+
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
